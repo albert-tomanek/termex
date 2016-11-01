d56f24e3a05aa70c8200a8f9de18d5323d3e4fa0
Albert Tomanek <electron826@gmail.com>
2016-11-01 20:15:55 +0000

    c11 - Added ASCII-only support.

--- chars.h
+++ chars.h
@@ -1,18 +1,35 @@
 #ifndef __TX_CHARS_H__
   #define __TX_CHARS_H__
+  
+  /* Uncomment the char set you wish to use */
+  
+  #define USE_UNICODE
+  //#define USE_ASCII
 
   // The letters signify the compas points to which the lines are pointing
   /* Box drawing chars  */
+  #ifdef USE_UNICODE
+    #define TB_BOX_ew 0x2500		// => S / N
+    #define TB_BOX_sw 0x2510		// => NE
+    #define TB_BOX_ns 0x2502		// => E / W
+    #define TB_BOX_nw 0x2518		// => SE
+    #define TB_BOX_ne 0x2514		// => SW
+    #define TB_BOX_se 0x250C		// => NW
 
-  #define TB_BOX_ew 0x2500		// => S / N
-  #define TB_BOX_sw 0x2510		// => NE
-  #define TB_BOX_ns 0x2502		// => E / W
-  #define TB_BOX_nw 0x2518		// => SE
-  #define TB_BOX_ne 0x2514		// => SW
-  #define TB_BOX_se 0x250C		// => NW
+    #define TB_CHAR_SPACE 0x0020
+    #define TB_CHAR_ENTER 0x21B5
+  #endif
+  
+  #ifdef USE_ASCII
+    #define TB_BOX_ew '-'		// => S / N
+    #define TB_BOX_sw '+'		// => NE
+    #define TB_BOX_ns '|'		// => E / W
+    #define TB_BOX_nw '+'		// => SE
+    #define TB_BOX_ne '+'		// => SW
+    #define TB_BOX_se '+'		// => NW
 
-  #define TB_CHAR_SPACE 0x0020
-  #define TB_CHAR_ENTER 0x21B5
-
+    #define TB_CHAR_SPACE ' '
+    #define TB_CHAR_ENTER '+'
+  #endif
 
 #endif
